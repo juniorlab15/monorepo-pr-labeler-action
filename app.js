0f6f8d57f406cafbab7893c641c14f2ab2664570
juniorlab15 <38108687+juniorlab15@users.noreply.github.com>
2019-10-03 17:28:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ async function prMonorepoRepoLabeler() {
     if (repo) {
       console.log(`labeling repo: ${repo}`)
 
-      const repoLabel = `service:${repo}`
+      const repoLabel = `:rocket:deploy:${repo}`
 
       helpers.addLabel(
         octokit,
